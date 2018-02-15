e92d2f84785788d6265ff575460f3a06d57fea2b
David <david@cloudwow.com>
2018-02-14 17:55:37 -0800

    dumbbot

--- compete.py
+++ compete.py
@@ -7,7 +7,7 @@ from random_bot import RandomBot
 sess = None  #tf.InteractiveSession()
 
 #player_1 = Bot(sess, "p1", 3, 3)
-player_1 = RandomBot()  #Bot(sess, "p2")
+player_1 = DumbBot()  #Bot(sess, "p2")
 player_2 = TreeBot(sess)
 #player_2 = RandomBot()
 #sess.run(tf.initialize_all_variables())
